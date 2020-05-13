e60dfdbd555fa3136358c853f84b5a63ca30db7c
Tom Petersen <petersen@informatik.uni-hamburg.de>
2020-05-13 10:20:03 +0200

    Reorder central methods and remove duplicates

--- threshold_crypto/central.py
+++ threshold_crypto/central.py
@@ -5,7 +5,7 @@ import nacl.exceptions
 import nacl.hash
 from threshold_crypto.data import KeyParameters, ThresholdParameters, PublicKey, KeyShare, PartialDecryption, \
     EncryptedMessage, ThresholdCryptoError, PartialReEncryptionKey, ReEncryptionKey
-from threshold_crypto import number
+from threshold_crypto import number, Participant
 
 
 # parameter generation
@@ -83,6 +83,39 @@ def create_public_key_and_shares_centralized(key_params: KeyParameters, threshol
     shares = [KeyShare(x, polynom.evaluate(x), key_params) for x in supporting_points]
 
     return public_key, shares
+
+
+def create_public_key(participants: [Participant], key_params: KeyParameters) -> PublicKey:
+    """
+    Pedersen91-related
+
+    :param participants:
+    :param key_params:
+    :return:
+    """
+    h = number.prod([p.h_i for p in participants]) % key_params.p
+    return PublicKey(h, key_params)
+
+
+def restore_priv_key(key_params: KeyParameters, shares: [KeyShare], treshold_params: ThresholdParameters):
+    """
+    Combine multiple key shares to compute the (implicit) private key.
+    Just used for testing purposes - should never be used in a real scenario, if you don't have a special reason for this!
+
+    :param key_params:
+    :param shares:
+    :param treshold_params:
+    :return:
+    """
+    used_shares = shares[:treshold_params.t]
+    x_shares = [share.x for share in used_shares]
+    y_shares = [share.y for share in used_shares]
+
+    lagrange_coefficients = number.build_lagrange_coefficients(x_shares, key_params.q)
+
+    restored_a = sum([(lagrange_coefficients[i] * y_shares[i]) for i in range(0, len(used_shares))]) % key_params.q
+
+    return restored_a
 
 
 # encryption
@@ -203,22 +236,6 @@ def _combine_shares(partial_decryptions: [PartialDecryption],
     return restored_m
 
 
-# private key restoration
-
-def restore_priv_key(key_params: KeyParameters, shares: [KeyShare], treshold_params: ThresholdParameters):
-    used_shares = shares[:treshold_params.t]
-    x_shares = [share.x for share in used_shares]
-    y_shares = [share.y for share in used_shares]
-
-    lagrange_coefficients = number.build_lagrange_coefficients(x_shares, key_params.q)
-    #tmp
-    print(lagrange_coefficients)
-
-    restored_a = sum([(lagrange_coefficients[i] * y_shares[i]) for i in range(0, len(used_shares))]) % key_params.q
-
-    return restored_a
-
-
 # re-encryption
 
 
@@ -256,11 +273,7 @@ def re_encrypt_message(em: EncryptedMessage, re_key: ReEncryptionKey) -> Encrypt
     return EncryptedMessage(em.v, re_enc_c, em.enc)
 
 
-
-
-# Pedersen91
-
-
+# TODO remove
 def initialize_participants(key_params: KeyParameters, threshold_params: ThresholdParameters) -> [Participant]:
     """
     Pedersen91-related
@@ -272,8 +285,9 @@ def initialize_participants(key_params: KeyParameters, threshold_params: Thresho
     return [Participant(p, key_params, threshold_params) for p in range(1, threshold_params.n + 1)]
 
 
+# TODO remove
 def create_private_shares(key_params: KeyParameters, threshold_params: ThresholdParameters,
-                          participants: List[Participant]) -> [KeyShare]:
+                          participants: [Participant]) -> [KeyShare]:
     """
     Pedersen91-related
 
@@ -284,22 +298,17 @@ def create_private_shares(key_params: KeyParameters, threshold_params: Threshold
     """
     shares = []
 
-    for p in participants:
-        p.choose_polynom()
+    for pi in participants:
+        for pj in participants:
+            pi.receive_F(pj.node_id, pj._local_F_ij)
 
+    node_ids = [p.node_id for p in participants]
     for p in participants:
-        p.compute_F()
+        p.calculate_sij(node_ids)
 
     for pi in participants:
         for pj in participants:
-            pi.receive_F(pj)
-
-    for p in participants:
-        p.calculate_sij(participants)
-
-    for pi in participants:
-        for pj in participants:
-            pi.receive_sij(pj)
+            pi.receive_sij(pj.node_id, pj._local_sij[pi.node_id])
 
     for p in participants:
         p.compute_share()
@@ -310,180 +319,4 @@ def create_private_shares(key_params: KeyParameters, threshold_params: Threshold
     return shares
 
 
-def create_public_key(participants: List[Participant], key_params: KeyParameters) -> PublicKey:
-    """
-    Pedersen91-related
 
-    :param participants:
-    :param key_params:
-    :return:
-    """
-    h = number.prod([p.h_i for p in participants]) % key_params.p
-    return PublicKey(h, key_params)
-
-
-def create_public_key_and_shares_centralized(participants: List[Participant], key_params: KeyParameters, threshold_params: ThresholdParameters) -> (
-PublicKey, [KeyShare]):
-    """
-    Creates a public key and n shares by choosing a random secret key and using it for computations.
-    :param key_params: key parameters to use
-    :param threshold_params: parameters t and n for the threshold scheme
-    :return: (the public key, n key shares)
-    """
-    """
-    a = number.getRandomRange(2, key_params.q - 2)
-    g_a = pow(key_params.g, a, key_params.p)
-    public_key = PublicKey(g_a, key_params)
-    """
-    a = 0
-    for i in range(len(participants)):
-        a += participants[i].a_i
-
-    g_a = 0
-    for i in range(len(participants)):
-        g_a += participants[i].h_i
-    public_key = PublicKey(g_a, key_params)
-
-    # Perform Shamir's secret sharing in Z_q
-    polynom = number.PolynomMod.create_random_polynom(a, threshold_params.t - 1, key_params.q)
-    supporting_points = range(1, threshold_params.n + 1)
-    shares = [KeyShare(x, polynom.evaluate(x), key_params) for x in supporting_points]
-
-    return public_key, shares
-
-
-def encrypt_message(message: str, public_key: PublicKey) -> EncryptedMessage:
-    """
-    Encrypt a message using a public key. A hybrid encryption approach is used to include advantages of symmetric
-    encryption (fast, independent of message-length, integrity-preserving by using AE-scheme).
-    Internally a combination of Salsa20 and Poly1305 from the cryptographic library NaCl is used.
-
-    :param message: the message to be encrypted
-    :param public_key: the public key
-    :return: an encrypted message
-    """
-    encoded_message = bytes(message, 'utf-8')
-    key_params = public_key.key_parameters
-
-    # Create random subgroup element and use its hash as symmetric key to prevent
-    # attacks described in "Why Textbook ElGamal and RSA Encryption Are Insecure"
-    # by Boneh et. al.
-    r = number.getRandomRange(2, public_key.key_parameters.q)
-    key_subgroup_element = pow(key_params.g, r, key_params.p)
-    key_subgroup_element_byte_length = (key_subgroup_element.bit_length() + 7) // 8
-    element_bytes = key_subgroup_element.to_bytes(key_subgroup_element_byte_length, byteorder='big')
-
-    try:
-        symmetric_key = nacl.hash.blake2b(element_bytes,
-                                          digest_size=nacl.secret.SecretBox.KEY_SIZE,
-                                          encoder=nacl.encoding.RawEncoder)
-        # Use derived symmetric key to encrypt the message
-        box = nacl.secret.SecretBox(symmetric_key)
-        encrypted = box.encrypt(encoded_message).hex()
-    except nacl.exceptions.CryptoError as e:
-        print('Encryption failed: ' + str(e))
-        raise ThresholdCryptoError('Message encryption failed.')
-
-    # Use threshold scheme to encrypt the subgroup element used as hash input to derive the symmetric key
-    g_k, c = ThresholdCrypto._encrypt_key_element(key_subgroup_element, public_key)
-
-    return EncryptedMessage(g_k, c, encrypted)
-
-
-def _encrypt_key_element(key_element: int, public_key: PublicKey) -> (int, int):
-    key_params = public_key.key_parameters
-
-    if key_element >= key_params.p:
-        raise ThresholdCryptoError('key element is larger than key parameter p')
-
-    k = number.getRandomRange(1, key_params.q - 1)
-    g_k = pow(key_params.g, k, key_params.p)  # aka v
-    g_ak = pow(public_key.g_a, k, key_params.p)
-    c = (key_element * g_ak) % key_params.p
-
-    return g_k, c
-
-
-def compute_partial_decryption(encrypted_message: EncryptedMessage, key_share: KeyShare) -> PartialDecryption:
-    """
-    Compute a partial decryption of an encrypted message using a key share.
-    :param encrypted_message: the encrypted message
-    :param key_share: the key share
-    :return: a partial decryption
-    """
-    key_params = key_share.key_parameters
-
-    v_y = pow(encrypted_message.v, key_share.y, key_params.p)
-
-    return PartialDecryption(key_share.x, v_y)
-
-
-def decrypt_message(partial_decryptions: [PartialDecryption],
-                    encrypted_message: EncryptedMessage,
-                    threshold_params: ThresholdParameters,
-                    key_params: KeyParameters
-                    ) -> str:
-    """
-    Decrypt a message using the combination of at least t partial decryptions. Similar to the encryption process
-    the hybrid approach is used for decryption.
-    :param partial_decryptions: at least t partial decryptions
-    :param encrypted_message: the encrapted message to be decrypted
-    :param threshold_params: the used threshold parameters
-    :param key_params: the used key parameters
-    :return: the decrypted message
-    """
-    key_subgroup_element = ThresholdCrypto._combine_shares(
-        partial_decryptions,
-        encrypted_message,
-        threshold_params,
-        key_params
-    )
-    key_subgroup_element_byte_length = (key_subgroup_element.bit_length() + 7) // 8
-    key_subgroup_element_bytes = key_subgroup_element.to_bytes(key_subgroup_element_byte_length, byteorder='big')
-
-    try:
-        key = nacl.hash.blake2b(key_subgroup_element_bytes,
-                                digest_size=nacl.secret.SecretBox.KEY_SIZE,
-                                encoder=nacl.encoding.RawEncoder)
-        box = nacl.secret.SecretBox(key)
-        encoded_plaintext = box.decrypt(bytes.fromhex(encrypted_message.enc))
-    except nacl.exceptions.CryptoError as e:
-        raise ThresholdCryptoError('Message decryption failed. Internal: ' + str(e))
-
-    return str(encoded_plaintext, 'utf-8')
-
-
-def restore_priv_key(key_params: KeyParameters, shares: [KeyShare], treshold_params: ThresholdParameters):
-    used_shares = shares[:treshold_params.t]
-    x_shares = [share.x for share in used_shares]
-    y_shares = [share.y for share in used_shares]
-
-    lagrange_coefficients = number.build_lagrange_coefficients(x_shares, key_params.q)
-
-    restored_a = sum([(lagrange_coefficients[i] * y_shares[i]) for i in range(0, len(used_shares))]) % key_params.q
-
-    return restored_a
-
-
-
-def _combine_shares(partial_decryptions: [PartialDecryption],
-                    encrypted_message: EncryptedMessage,
-                    threshold_params: ThresholdParameters,
-                    key_params: KeyParameters
-                    ) -> int:
-    # Disabled to enable testing for unsuccessful decryption
-    #if len(partial_decryptions) < threshold_params.t:
-    #    raise ThresholdCryptoError('less than t partial decryptions given')
-
-    # compute lagrange coefficients
-    partial_indices = [dec.x for dec in partial_decryptions]
-    lagrange_coefficients = number.build_lagrange_coefficients(partial_indices, key_params.q)
-    factors = [
-        pow(partial_decryptions[i].v_y, lagrange_coefficients[i], key_params.p)
-        for i in range(0, len(partial_decryptions))
-    ]
-    restored_g_ka = number.prod(factors) % key_params.p
-    restored_g_minus_ak = number.prime_mod_inv(restored_g_ka, key_params.p)
-    restored_m = encrypted_message.c * restored_g_minus_ak % key_params.p
-
-    return restored_m
