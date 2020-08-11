1e05f5e7cf6ae4d7d5aa8357c599817c2e420975
Tom Petersen <petersen@informatik.uni-hamburg.de>
2020-08-11 11:07:13 +0200

    Adjust TcTestCase

--- test/tests.py
+++ test/tests.py
@@ -1,15 +1,15 @@
 import unittest
 
 from threshold_crypto.data import (ThresholdParameters,
-                                   KeyParameters,
+                                   CurveParameters,
                                    ThresholdCryptoError,
-                                   PublicKey,
                                    KeyShare,
                                    EncryptedMessage,
                                    PartialDecryption,
+                                   PublicKey,
                                    )
-from threshold_crypto import number
-from threshold_crypto import central, Participant
+from threshold_crypto import number, PartialReEncryptionKey, ReEncryptionKey
+from threshold_crypto import central
 from threshold_crypto import participant
 
 
@@ -17,8 +17,8 @@ class TCTestCase(unittest.TestCase):
 
     def setUp(self):
         self.tp = ThresholdParameters(3, 5)
-        self.kp = central.static_512_key_parameters()
-        self.pk, self.shares = central.create_public_key_and_shares_centralized(self.kp, self.tp)
+        self.cp = CurveParameters()
+        self.pk, self.shares = central.create_public_key_and_shares_centralized(self.cp, self.tp)
         self.message = 'Some secret message'
         self.em = central.encrypt_message(self.message, self.pk)
         self.reconstruct_shares = [self.shares[i] for i in [0, 2, 4]]  # choose 3 of 5 key shares
@@ -28,11 +28,11 @@ class TCTestCase(unittest.TestCase):
         pass
 
     def test_valid_threshold_parameters(self):
-        t = ThresholdParameters(3, 5)
+        self.assertTrue(ThresholdParameters(3, 5))
 
     def test_invalid_threshold_parameters(self):
         with self.assertRaises(ThresholdCryptoError):
-            t = ThresholdParameters(5, 3)
+            ThresholdParameters(5, 3)
 
     def test_threshold_parameter_json(self):
         t = ThresholdParameters(3, 5)
@@ -40,47 +40,23 @@ class TCTestCase(unittest.TestCase):
 
         self.assertEqual(t, t_j)
 
-    def test_valid_key_parameters(self):
-        k = KeyParameters(7, 3, 2)  # 2 generates 3-order subgroup {1,2,4}
+    def test_valid_curve_parameters(self):
+        cp = CurveParameters()
+        self.assertTrue(cp.order > 0)
 
-    def test_invalid_key_parameters_whole_group(self):
+    def test_invalid_curve_parameters_whole_group(self):
         with self.assertRaises(ThresholdCryptoError):
-            k = KeyParameters(7, 3, 3)  # 3 generates 6-order group Z_7*
+            CurveParameters(curve_name="invalid-curve")
 
-    def test_invalid_key_parameters_no_safe_prime(self):
-        with self.assertRaises(ThresholdCryptoError):
-            k = KeyParameters(7, 4, 3)
+    def test_curve_parameter_json(self):
+        cp = CurveParameters()
+        cp_j = CurveParameters.from_json(cp.to_json())
 
-    def test_key_parameter_json(self):
-        k_j = KeyParameters.from_json(self.kp.to_json())
-
-        self.assertEqual(self.kp, k_j)
-
-    def test_static_512_key_parameters(self):
-        kp = central.static_512_key_parameters()
-
-        self.assertEqual(kp.p, 2*kp.q + 1)  # safe prime
-        self.assertEqual(pow(kp.g, kp.q, kp.p), 1)  # g generates q order subgroup
-        self.assertNotEqual(pow(kp.g, 2, kp.p), 1)
-
-    def test_static_1024_key_parameters(self):
-        kp = central.static_1024_key_parameters()
-
-        self.assertEqual(kp.p, 2*kp.q + 1)  # safe prime
-        self.assertEqual(pow(kp.g, kp.q, kp.p), 1)  # g generates q order subgroup
-        self.assertNotEqual(pow(kp.g, 2, kp.p), 1)
-
-    def test_static_2048key_parameters(self):
-        kp = central.static_2048_key_parameters()
-
-        self.assertEqual(kp.p, 2 * kp.q + 1)  # safe prime
-        self.assertEqual(pow(kp.g, kp.q, kp.p), 1)  # g generates q order subgroup
-        self.assertNotEqual(pow(kp.g, 2, kp.p), 1)
+        self.assertEqual(cp, cp_j)
 
     def test_central_key_generation(self):
-        pk, shares = central.create_public_key_and_shares_centralized(self.kp, self.tp)
+        pk, shares = central.create_public_key_and_shares_centralized(self.cp, self.tp)
 
-        self.assertEqual(pk.key_parameters, self.kp)
         self.assertEqual(len(shares), self.tp.n)
 
     def test_public_key_json(self):
@@ -97,8 +73,9 @@ class TCTestCase(unittest.TestCase):
     def test_message_encryption(self):
         em = central.encrypt_message(self.message, self.pk)
 
-        self.assertTrue(em.c >= 0)
-        self.assertTrue(em.v >= 0)
+        self.assertTrue(em.C1)
+        self.assertTrue(em.C2)
+        self.assertTrue(em.ciphertext)
 
     def test_message_json(self):
         m_j = EncryptedMessage.from_json(self.em.to_json())
@@ -120,47 +97,47 @@ class TCTestCase(unittest.TestCase):
         self.assertTrue(p.evaluate(0) == 17)
 
     def test_key_encryption_decryption_with_enough_shares(self):
-        r = number.getRandomRange(2, self.kp.q)
-        testkey_element = pow(self.kp.g, r, self.kp.p)
-        g_k, c = central._encrypt_key_element(testkey_element, self.pk)
-        em = EncryptedMessage(g_k, c, '')
+        r = number.getRandomRange(2, self.cp.order)
+        testkey_element = r * self.cp.P
+        kP, c = central._encrypt_key_point(testkey_element, self.pk.Q, self.cp)
+        em = EncryptedMessage(kP, c, b'')
         reconstruct_shares = [self.shares[i] for i in [0, 2, 4]]  # choose 3 of 5 key shares
         partial_decryptions = [participant.compute_partial_decryption(em, share) for share in reconstruct_shares]
-        rec_testkey_element = central._combine_shares(partial_decryptions, em, self.tp, self.kp)
+        rec_testkey_element = central._combine_shares(partial_decryptions, em, self.tp, self.cp)
 
         self.assertEqual(testkey_element, rec_testkey_element)
 
     def test_key_encryption_decryption_without_enough_shares(self):
-        r = number.getRandomRange(2, self.kp.q)
-        testkey_element = pow(self.kp.g, r, self.kp.p)
-        g_k, c = central._encrypt_key_element(testkey_element, self.pk)
-        em = EncryptedMessage(g_k, c, '')
+        r = number.getRandomRange(2, self.cp.order)
+        testkey_element = r * self.cp.P
+        kP, c = central._encrypt_key_point(testkey_element, self.pk.Q, self.cp)
+        em = EncryptedMessage(kP, c, b'')
         reconstruct_shares = [self.shares[i] for i in [0, 4]]  # choose 2 of 5 key shares
         partial_decryptions = [participant.compute_partial_decryption(em, share) for share in reconstruct_shares]
-        rec_testkey_element = central._combine_shares(partial_decryptions, em, self.tp, self.kp)
+        rec_testkey_element = central._combine_shares(partial_decryptions, em, self.tp, self.cp)
 
         self.assertNotEqual(testkey_element, rec_testkey_element)
 
     def test_complete_process_with_enough_shares(self):
-        key_params = central.static_512_key_parameters()
+        curve_params = CurveParameters()
         thresh_params = ThresholdParameters(3, 5)
 
-        pub_key, key_shares = central.create_public_key_and_shares_centralized(key_params, thresh_params)
+        pub_key, key_shares = central.create_public_key_and_shares_centralized(curve_params, thresh_params)
 
         message = 'Some secret message to be encrypted!'
         encrypted_message = central.encrypt_message(message, pub_key)
 
         reconstruct_shares = [key_shares[i] for i in [0, 2, 4]]  # choose 3 of 5 key shares
         partial_decryptions = [participant.compute_partial_decryption(encrypted_message, share) for share in reconstruct_shares]
-        decrypted_message = central.decrypt_message(partial_decryptions, encrypted_message, thresh_params, key_params)
+        decrypted_message = central.decrypt_message(partial_decryptions, encrypted_message, thresh_params)
 
         self.assertEqual(message, decrypted_message)
 
     def test_complete_process_without_enough_shares(self):
-        key_params = central.static_512_key_parameters()
+        curve_params = CurveParameters()
         thresh_params = ThresholdParameters(3, 5)
 
-        pub_key, key_shares = central.create_public_key_and_shares_centralized(key_params, thresh_params)
+        pub_key, key_shares = central.create_public_key_and_shares_centralized(curve_params, thresh_params)
 
         message = 'Some secret message to be encrypted!'
         encrypted_message = central.encrypt_message(message, pub_key)
@@ -169,7 +146,7 @@ class TCTestCase(unittest.TestCase):
         partial_decryptions = [participant.compute_partial_decryption(encrypted_message, share) for share in reconstruct_shares]
 
         with self.assertRaises(ThresholdCryptoError):
-            central.decrypt_message(partial_decryptions, encrypted_message, thresh_params, key_params)
+            central.decrypt_message(partial_decryptions, encrypted_message, thresh_params)
 
 
 class PreTestCase(unittest.TestCase):
